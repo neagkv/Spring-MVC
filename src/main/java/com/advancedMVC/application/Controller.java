8036d6d1683fe2af239b5a9fdcf5b6dab8305a59
Kevin Neag <neagkv@gmail.com>
2018-02-09 01:01:59 -0500

    mapping

--- src/main/java/com/advancedMVC/application/Controller.java
+++ src/main/java/com/advancedMVC/application/Controller.java
@@ -1,8 +1,13 @@
 package com.advancedMVC.application;
 
+import com.advancedMVC.application.model.Users;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import javax.validation.Valid;
 
 /**
  * @author Kevin Neag
@@ -20,6 +25,47 @@ public class Controller {
 	public ModelAndView register(){
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+
+	@RequestMapping(value="/admin/greetings", method = RequestMethod.GET)
+	public ModelAndView greetings(){
+		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		Users users = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("userName", "Welcome " + users.getFirstname() + " " + users.getLastname() + " (" + user.getEmail() + ")");
+		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+		modelAndView.setViewName("admin/greetings");
+		return modelAndView;
+	}
+
+	@RequestMapping(value="/registration", method = RequestMethod.GET)
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		Users user = new Users();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("registration");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+	public ModelAndView createNewUser(@Valid Users user, BindingResult bindingResult) {
+		ModelAndView modelAndView = new ModelAndView();
+		Users userExists = userService.findUserByEmail(user.getEmail());
+		if (userExists != null) {
+			bindingResult
+					.rejectValue("email", "error.user",
+							"There is already a user registered with the email provided");
+		}
+		if (bindingResult.hasErrors()) {
+			modelAndView.setViewName("registration");
+		} else {
+			userService.saveUser(user);
+			modelAndView.addObject("successMessage", "User has been registered successfully");
+			modelAndView.addObject("user", new Users());
+			modelAndView.setViewName("registration");
+
+		}
 		return modelAndView;
 	}
 }
--- src/main/java/com/advancedMVC/application/service/UserServiceInterface.java
+++ src/main/java/com/advancedMVC/application/service/UserServiceInterface.java
@@ -1,28 +1,15 @@
 package com.advancedMVC.application.service;
 
 import com.advancedMVC.application.model.Users;
-import com.advancedMVC.application.repository.RoleRepo;
-import com.advancedMVC.application.repository.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 /**
  * @author Kevin Neag
  */
 public interface UserServiceInterface {
 
-    @Autowired
-    UserRepo userRepos;
+    public Users findUserByEmail(String email);
+    public void saveUser(Users user);
 
-    @Autowired
-    RoleRepo roleRepo;
-
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Override
-    public Users findUserByEmail(String email) {
-        return userRepo.findByEmail(email);
-    }
 
 }
